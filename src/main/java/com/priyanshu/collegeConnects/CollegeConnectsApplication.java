6d85672a006d0a920266075e0f4439aa4d266e71
Priyanshu Gupta <priyanshug1999@gmail.com>
2023-03-30 18:50:15 +0530

    initialize project

---
+++ src/main/java/com/priyanshu/collegeConnects/CollegeConnectsApplication.java
@@ -0,0 +1,14 @@
+package com.priyanshu.collegeConnects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeConnectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeConnectsApplication.class, args);
+		System.out.println("\nCollege connects running\n");
+	}
+
+}
