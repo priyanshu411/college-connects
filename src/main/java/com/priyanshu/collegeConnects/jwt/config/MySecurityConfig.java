7a30c808d247b68ec12e93337a8f6b9fb508e702
Priyanshu Gupta <priyanshug1999@gmail.com>
2023-04-11 12:46:42 +0530

    Cors configuration

--- src/main/java/com/priyanshu/collegeConnects/jwt/config/MySecurityConfig.java
+++ src/main/java/com/priyanshu/collegeConnects/jwt/config/MySecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -26,10 +31,11 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .cors()
-                .disable()
+
+                .cors() // enable cors configuration
+                .and()
+                .csrf().disable()
+                // rest of your security configuration
                 .authorizeRequests()
                 .antMatchers("/token").permitAll()
                 .anyRequest().authenticated()
@@ -51,4 +57,16 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // allow all origins
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","OPTIONS")); // allow specific HTTP methods
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // allow specific headers
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
