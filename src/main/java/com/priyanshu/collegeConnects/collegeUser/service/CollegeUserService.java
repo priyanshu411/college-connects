c34aad954a4de4eb56d40322bb735fbc0269c8eb
Priyanshu Gupta <priyanshug1999@gmail.com>
2023-04-21 01:46:05 +0530

    Update CollegeUserService.java

--- src/main/java/com/priyanshu/collegeConnects/collegeUser/service/CollegeUserService.java
+++ src/main/java/com/priyanshu/collegeConnects/collegeUser/service/CollegeUserService.java
@@ -2,6 +2,7 @@ package com.priyanshu.collegeConnects.collegeUser.service;
 
 import com.priyanshu.collegeConnects.database.entity.CollegeUser;
 import com.priyanshu.collegeConnects.database.repository.ICollegeUser;
+import com.priyanshu.collegeConnects.exception.UserAlreadyPresentException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +15,11 @@ public class CollegeUserService {
 
 
     public CollegeUser createCollegeUser(CollegeUser user){
-       repository.save(user);
-       return user;
+        if(repository.findById(user.getEnrollmentNo()).isPresent() ||repository.findByEmail(user.getEmail()).isPresent()){
+            throw new UserAlreadyPresentException(user.getEnrollmentNo(),"User already present with this enrollment No or email");
+        }
+        repository.save(user);
+        return user;
     }
     public Optional<CollegeUser> getUser(String enrollmentNo){
         Optional<CollegeUser> user=repository.findById(enrollmentNo);
