766773d78046b00e70bcd509ae6f97f12c042fa1
Priyanshu Gupta <priyanshug1999@gmail.com>
2023-04-21 01:46:15 +0530

    Update ICollegeUser.java

--- src/main/java/com/priyanshu/collegeConnects/database/repository/ICollegeUser.java
+++ src/main/java/com/priyanshu/collegeConnects/database/repository/ICollegeUser.java
@@ -4,6 +4,9 @@ import com.priyanshu.collegeConnects.database.entity.CollegeUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ICollegeUser extends JpaRepository<CollegeUser,String> {
+    Optional<CollegeUser> findByEmail(String email);
 }
