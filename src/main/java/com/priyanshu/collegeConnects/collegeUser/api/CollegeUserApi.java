9043680dc37b2e57b836c6862d6d219a8d43a1a6
Priyanshu Gupta <priyanshug1999@gmail.com>
2023-04-28 01:58:30 +0530

    Update CollegeUserApi.java

--- src/main/java/com/priyanshu/collegeConnects/collegeUser/api/CollegeUserApi.java
+++ src/main/java/com/priyanshu/collegeConnects/collegeUser/api/CollegeUserApi.java
@@ -1,20 +1,29 @@
 package com.priyanshu.collegeConnects.collegeUser.api;
 
+import com.priyanshu.collegeConnects.collegeUser.dto.CollegeUserSummaryDto;
 import com.priyanshu.collegeConnects.collegeUser.service.CollegeUserService;
 import com.priyanshu.collegeConnects.database.entity.CollegeUser;
+import com.priyanshu.collegeConnects.jwt.jwtHelper.JwtUtil;
+import com.priyanshu.collegeConnects.response.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api")
 public class CollegeUserApi {
 @Autowired
     private CollegeUserService collegeUserService;
-    @PostMapping(value = "user/create")
-    public CollegeUser CreateCollegeUser(@RequestBody CollegeUser collegeUser){
+    @Autowired
+    private JwtUtil jwtUtil;
+    @PostMapping(value = "/user/create")
+    public ApiResponse CreateCollegeUser(@RequestBody CollegeUser collegeUser){
         return collegeUserService.createCollegeUser(collegeUser);
+    }
+
+    @GetMapping(value = "/user")
+    public CollegeUserSummaryDto getUser(@RequestHeader("Authorization") String authorization){
+        String jwtToken=authorization.substring(7);
+        return collegeUserService.getUserSummary(jwtUtil.extractUsername(jwtToken));
+
     }
 }
