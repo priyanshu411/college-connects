fc56027c2393829097277190118d8a60537c8dc8
Priyanshu Gupta <priyanshug1999@gmail.com>
2023-04-28 01:58:38 +0530

    Update CollegeUserService.java

--- src/main/java/com/priyanshu/collegeConnects/collegeUser/service/CollegeUserService.java
+++ src/main/java/com/priyanshu/collegeConnects/collegeUser/service/CollegeUserService.java
@@ -1,8 +1,11 @@
 package com.priyanshu.collegeConnects.collegeUser.service;
 
+import com.priyanshu.collegeConnects.collegeUser.dto.CollegeUserSummaryDto;
 import com.priyanshu.collegeConnects.database.entity.CollegeUser;
 import com.priyanshu.collegeConnects.database.repository.ICollegeUser;
 import com.priyanshu.collegeConnects.exception.UserAlreadyPresentException;
+import com.priyanshu.collegeConnects.exception.UserNotFoundException;
+import com.priyanshu.collegeConnects.response.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +17,33 @@ public class CollegeUserService {
     private ICollegeUser repository;
 
 
-    public CollegeUser createCollegeUser(CollegeUser user){
+    public ApiResponse createCollegeUser(CollegeUser user){
         if(repository.findById(user.getEnrollmentNo()).isPresent() ||repository.findByEmail(user.getEmail()).isPresent()){
             throw new UserAlreadyPresentException(user.getEnrollmentNo(),"User already present with this enrollment No or email");
         }
         repository.save(user);
-        return user;
+        return new ApiResponse(user.getEnrollmentNo(),"Success","Account created successfully");
     }
     public Optional<CollegeUser> getUser(String enrollmentNo){
         Optional<CollegeUser> user=repository.findById(enrollmentNo);
         return user;
+
+    }
+    public CollegeUserSummaryDto getUserSummary(String enrollmentNo ){
+        Optional<CollegeUser> user=repository.findById(enrollmentNo);
+        if(user.isPresent()){
+            CollegeUserSummaryDto collegeUserSummaryDto=CollegeUserSummaryDto.builder()
+                    .enrollmentNo(user.get().getEnrollmentNo())
+                    .userName(user.get().getUserName())
+                    .course(user.get().getCourse())
+                    .semester(user.get().getSemester())
+                    .userType(user.get().getUserType())
+                    .passOutYear(user.get().getPassOutYear())
+                    .build();
+            return collegeUserSummaryDto;
+        }else{
+            throw new UserNotFoundException(enrollmentNo,"user not found");
+        }
 
     }
 }
