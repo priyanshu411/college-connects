9129453ca163e7907ba2da54ca00826e2d329c6e
Priyanshu Gupta <priyanshug1999@gmail.com>
2023-05-07 02:46:21 +0530

    Update UserAlreadyPresentException.java

--- src/main/java/com/priyanshu/collegeConnects/exception/UserAlreadyPresentException.java
+++ src/main/java/com/priyanshu/collegeConnects/exception/UserAlreadyPresentException.java
@@ -1,8 +1,5 @@
 package com.priyanshu.collegeConnects.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
 public class UserAlreadyPresentException extends CustomException{
     public UserAlreadyPresentException(String id,String message){
